d9a426716d9917b12cd5c2ddfe6a7d211ffca24f
prabath.gamage <prabath.gamage@platform1.net>
2019-02-26 11:49:39 +0530

    v3.0.1 first option exclusive - DK

--- resources/dynamic/standard.js
+++ resources/dynamic/standard.js
@@ -17,7 +17,27 @@ $(window).load(function() {
 		showRankMoveControls: {%= (CurrentADC.PropValue("showRankMoveControls") = "1") %},
 		controlAlign : '{%= CurrentADC.PropValue("controlAlign") %}',
 		setMax : parseInt('{%= CurrentADC.PropValue("maxRankedItems") %}'),
-		dkActivated : {% If (CurrentQuestion.Type = "multiple") Then %}{% If (CurrentQuestion.AvailableResponses[CurrentQuestion.AvailableResponses.Count].IsExclusive and CurrentQuestion.AvailableResponses[CurrentQuestion.AvailableResponses.Count - 1].IsExclusive) Then %}2{% ElseIf (CurrentQuestion.AvailableResponses[CurrentQuestion.AvailableResponses.Count].IsExclusive) Then %}1{% Else %}0{% EndIf %}{% Else %}{% If (CurrentADC.PropValue("dkActivated") = "2") Then %}2{% ElseIf (CurrentADC.PropValue("dkActivated") = "1") Then %}1{% Else %}0{% EndIf %}{% EndIf %},
+		dkActivated : {% If (CurrentQuestion.Type = "multiple") Then %}
+										{% If (CurrentQuestion.AvailableResponses[1].IsExclusive) Then %}
+											3
+										{% ElseIf (CurrentQuestion.AvailableResponses[CurrentQuestion.AvailableResponses.Count].IsExclusive and CurrentQuestion.AvailableResponses[CurrentQuestion.AvailableResponses.Count - 1].IsExclusive) Then %}
+											2
+										{% ElseIf (CurrentQuestion.AvailableResponses[CurrentQuestion.AvailableResponses.Count].IsExclusive) Then %}
+											1
+										{% ElseIf (CurrentQuestion.AvailableResponses[CurrentQuestion.AvailableResponses.Count].IsExclusive) Then %}
+
+										{% Else %}
+											0
+										{% EndIf %}
+									{% Else %}
+										{% If (CurrentADC.PropValue("dkActivated") = "2") Then %}
+											2
+										{% ElseIf (CurrentADC.PropValue("dkActivated") = "1") Then %}
+											1
+										{% Else %}
+											0
+										{% EndIf %}
+									{% EndIf %},
 		animatedResponses : {%= (CurrentADC.PropValue("animatedResponses") = "1") %},
 		layout : '{%= CurrentADC.PropValue("responseLayout") %}',
       	currentQuestion: '{%:= CurrentQuestion.Shortcut %}',
