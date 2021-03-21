d5935fd30e9d38b75f515bc93e830b1c85672e60
jeromed <jeromed@askia.com>
2021-03-21 15:29:18 +0100

    v4.0.0
    Changed GUID cause duplicate with RankingIsotopeOther and same code as this one now

--- resources/dynamic/standard.js
+++ resources/dynamic/standard.js
@@ -1,5 +1,25 @@
 /* standard_default.js */
 $(window).load(function() {
+	{%
+	Dim i
+
+	Dim strOtherRID = ""
+	Dim strOtherQID = ""
+
+	Dim ar = CurrentQuestion.AvailableResponses
+	For i = 1 to ar.Count
+	    If ar[i].isOpen = True Then
+	        If strOtherRID <> "" Then
+	          strOtherRID = strOtherRID + ","
+	            strOtherQID = strOtherQID + ","
+	        Endif
+	        strOtherRID = strOtherRID + ar[i].Index
+	        strOtherQID = strOtherQID + ar[i].OpenQuestion.InputName()
+	    Endif
+	Next i
+
+	%}
+
 	$('#adc_{%= CurrentADC.InstanceId %}').adcRanking({
         instanceId : '{%= CurrentADC.InstanceId%}',
 		maxWidth : '{%= CurrentADC.PropValue("maxWidth") %}',
@@ -16,38 +36,24 @@ $(window).load(function() {
 		showResponseHoverBorder: {%= (CurrentADC.PropValue("showResponseHoverBorder") = "1") %},
 		showRankMoveControls: {%= (CurrentADC.PropValue("showRankMoveControls") = "1") %},
 		controlAlign : '{%= CurrentADC.PropValue("controlAlign") %}',
+		{% If (CurrentQuestion.Type = "numeric" ) Then %}
+			otherRID : '{%= CurrentADC.PropValue("otherRID") %}',
+			otherQID : '{%= CurrentADC.PropValue("otherQID") %}',
+		{% Else %}
+			otherRID : '{%= strOtherRID %}',
+			otherQID : '{%= strOtherQID %}',
+		{% EndIf %}
 		setMax : parseInt('{%= CurrentADC.PropValue("maxRankedItems") %}'),
-		dkActivated : {% If (CurrentQuestion.Type = "multiple") Then %}
-											{% If (CurrentQuestion.AvailableResponses[1].IsExclusive) Then %}
-												3
-											{% ElseIf (CurrentQuestion.AvailableResponses[CurrentQuestion.AvailableResponses.Count].IsExclusive and CurrentQuestion.AvailableResponses[CurrentQuestion.AvailableResponses.Count - 1].IsExclusive) Then %}
-												2
-											{% ElseIf (CurrentQuestion.AvailableResponses[CurrentQuestion.AvailableResponses.Count].IsExclusive) Then %}
-												1
-											{% Else %}
-												0
-											{% EndIf %}
-										{% Else %}
-											{% If (CurrentADC.PropValue("dkActivated") = "3") Then %}
-												3
-											{% ElseIf (CurrentADC.PropValue("dkActivated") = "2") Then %}
-												2
-											{% ElseIf (CurrentADC.PropValue("dkActivated") = "1") Then %}
-												1
-											{% Else %}
-												0
-											{% EndIf %}
-										{% EndIf %},
+		dkActivated : {% If (CurrentQuestion.Type = "multiple") Then %}{% If (CurrentQuestion.AvailableResponses[CurrentQuestion.AvailableResponses.Count].IsExclusive and CurrentQuestion.AvailableResponses[CurrentQuestion.AvailableResponses.Count - 1].IsExclusive) Then %}2{% ElseIf (CurrentQuestion.AvailableResponses[CurrentQuestion.AvailableResponses.Count].IsExclusive) Then %}1{% Else %}0{% EndIf %}{% Else %}{% If (CurrentADC.PropValue("dkActivated") = "2") Then %}2{% ElseIf (CurrentADC.PropValue("dkActivated") = "1") Then %}1{% Else %}0{% EndIf %}{% EndIf %},
 		animatedResponses : {%= (CurrentADC.PropValue("animatedResponses") = "1") %},
 		layout : '{%= CurrentADC.PropValue("responseLayout") %}',
       	currentQuestion: '{%:= CurrentQuestion.Shortcut %}',
-      	type: '{%:= CurrentQuestion.Type %}',
 		items : [
-      {% If (CurrentQuestion.Type = "numeric" ) Then %}
-      {%:= CurrentADC.GetContent("dynamic/standard_numeric.js").ToText()%}
-      {% Else %}
-      {%:= CurrentADC.GetContent("dynamic/standard_multiple.js").ToText()%}
-      {% EndIf %}
+      		{% If (CurrentQuestion.Type = "numeric" ) Then %}
+			{%:= CurrentADC.GetContent("dynamic/standard_numeric.js").ToText()%}
+      		{% Else %}
+      		{%:= CurrentADC.GetContent("dynamic/standard_multiple.js").ToText()%}
+      		{% EndIf %}
 		]
 	});
 });
--- resources/static/ranking.js
+++ resources/static/ranking.js
@@ -3,29 +3,42 @@
 
 	$.fn.adcRanking = function adcRanking(options) {
 
-		// 2 MS: Syntax to set the default value or use the one specified
+		// MS: Syntax to set the default value or use the one specified
         (options.instanceId = options.instanceId || 1);
 		(options.width = options.width || "auto");
 		(options.height = options.height || "auto");
 		(options.setMax = options.setMax || $(this).children('.statement').size());
         (options.currentQuestion = options.currentQuestion || '');
-        (options.type = options.type || 'numeric');
-
+		
+		var otherQIDarray = options.otherQID.split(","),
+			otherRIDarray = options.otherRID.split(",");
+		
 		$(this).css({'max-width':options.maxWidth,'width':options.controlWidth});
 		$(this).parents('.controlContainer').css({'width':'100%','overflow':'hidden'});
-
+		
 		if ( options.controlAlign === "center" ) {
 			$(this).parents('.controlContainer').css({'text-align':'center'});
 			$(this).css({'margin':'0px auto'});
 		} else if ( options.controlAlign === "right" ) {
 			$(this).css({'margin':'0 0 0 auto'});
 		}
-
+		
 		var dkActivated = options.dkActivated;
 		var animatedResponses = options.animatedResponses;
 		var layout = options.layout;
 		var dkselected = false;
-
+		
+		// Other
+		$(this).parents('.controlContainer').find('.statement .otherText').width( $(this).find('.statement').innerWidth() - 30 ).hide();
+		//OLD if ( $( '#'+options.otherQID ).val() !== '' ) $(this).parents('.controlContainer').find('.otherText').val( $( '#'+options.otherQID ).val() );
+		//OLD $( '#'+options.otherQID ).hide();
+		var i;
+		for (i = 0; i < otherQIDarray.length; ++i) {
+			if ( $( '#'+otherQIDarray[i] ).val() !== '' ) 
+				$(this).parents('.controlContainer').find('.statement[data-index="'+otherRIDarray[i]+'"] .otherText').val( $( '#'+otherQIDarray[i] ).val()).show();
+			$( '#'+otherQIDarray[i] ).hide();
+		}
+		
 		// List of items collection of object {caption : 'The caption', element : jQueryObject}
 		var $container = $(this),
 			items = options.items,
@@ -49,39 +62,39 @@
 		// Select a statement
 		// @this = target node
 		function selectStatement(e) {
-
+			
 			//console.log ( (!$(e.target).hasClass('upRank') && !$(e.target).hasClass('downRank')) + ":" + !$(e.target).hasClass('upRank') +":"+ !$(e.target).hasClass('downRank'));
 			if ( !$(e.target).hasClass('upRank') && !$(e.target).hasClass('downRank') ) {
-
+			
 				var $target = $(this),
 					index	= $target.data('index'),
 					$input = items[index].element,
 					value	= parseInt($input.val(), 10);
 
 				if (!value) { // item is currently UNranked
-				if (((index+1) == items.length && (dkActivated == 1 || dkActivated == 2)) || ((index+1) == (items.length - 1) && dkActivated == 2) || (index == 0 && dkActivated == 3)) {
+					if (((index+1) == items.length && (dkActivated == 1 || dkActivated == 2)) || ((index+1) == (items.length - 1) && dkActivated == 2)) {
 						deselectAllStatements();
 						dkselected = true;
+						
+						if ((rankCount + 1) <= options.setMax) {
+							rankCount += 1;
+							$input.val(rankCount);
 
-						if (dkActivated == 3) {
-							if ((rankCount + 1) <= options.setMax) {
-								rankCount += 1;
-								$input.val(rankCount);
-
-								$target.addClass('ranked') // Add ranked class
-									.find('.rank_text').html(''); // Add rank value
+							$target.addClass('ranked')                // Add ranked class
+								.find('.rank_text').html(''); // Add rank value
+								
+							$(this).parents('.controlContainer').find('.otherText').val('');
+							for (i = 0; i < otherQIDarray.length; ++i) {
+								$( '#'+otherQIDarray[i] ).val('');
 							}
-						} else {
-							if ((rankCount + 1) <= options.setMax) {
-								rankCount += 1;
-								$input.val(rankCount);
-
-								$target.addClass('ranked') // Add ranked class
-									.find('.rank_text').html(''); // Add rank value
-							}
+							$(this).parents('.controlContainer').find('.otherText').hide();
+							
+							/*var otherID = $.inArray( String(parseInt($(this).attr('data-index'))), otherRIDarray );
+							if ( $.inArray( String(parseInt($(this).attr('data-index'))), otherRIDarray ) != -1 ) 
+								$(this).find('.otherText').show().focus();*/
 						}
 					} else {
-						if (dkselected){
+						if (dkselected){ 
 							deselectAllStatements();
 						}
 						if ((rankCount + 1) <= options.setMax) {
@@ -90,8 +103,13 @@
 
 							$target.addClass('ranked')                // Add ranked class
 								.find('.rank_text').html(rankCount); // Add rank value
+								
+							var otherID = $.inArray( String(parseInt($(this).attr('data-index'))), otherRIDarray );
+							if ( $.inArray( String(parseInt($(this).attr('data-index'))), otherRIDarray ) != -1 ) 
+								$(this).find('.otherText').show().focus();
 						}
 					}
+						
 				} else { // item is currently ranked
 
 					rankCount -= 1;
@@ -101,7 +119,7 @@
 						.find('.rank_text').html(''); // Remove rank value
 
 					// Rerank other items
-					$container.find('.statement').each(function() {
+					$container.find('.statement').each(function() { 
 						var $current = $(this),
 							currentItem  = items[$current.data('index')],
 							currentValue = parseInt(currentItem.element.val(), 10);
@@ -113,6 +131,16 @@
 						}
 
 					});
+					
+					if ( $.inArray( String(parseInt($(this).attr('data-index'))), otherRIDarray ) != -1 ) {
+						
+						//FIX
+						var otherID = $.inArray( String(parseInt($(this).attr('data-index'))), otherRIDarray );
+						$(this).find('.otherText').hide();
+						$(this).find('.otherText').val('');
+						$( '#'+otherQIDarray[otherID] ).val('');
+					}
+
 
 				}
 
@@ -121,28 +149,26 @@
 					$container.isotope('updateSortData', $container.find('.statement'));
 					$container.isotope(istopeOptions);
 				}
-
-                if (window.askia
-                    && window.arrLiveRoutingShortcut
+                if (window.askia 
+                    && window.arrLiveRoutingShortcut 
                     && window.arrLiveRoutingShortcut.length > 0
                     && window.arrLiveRoutingShortcut.indexOf(options.currentQuestion) >= 0) {
                     askia.triggerAnswer();
                 }
-
 			}
-
+			
 			/*if ( showRankMoveControls && (navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i)) ) {
 				//showMoveOptions();
 			}*/
 		}
-
+		
 		// Select a statement
 		// @this = target node
 		function deselectAllStatements() {
 			dkselected = false;
 			//$container.on('click', '.statement', selectStatement);
 			$container.find( '.statement' ).each(function(i, obj) {
-
+			
 				var $target = $(obj),
 					index	= $target.data('index'),
 					$input = items[index].element,
@@ -188,9 +214,9 @@
 					$container.isotope(istopeOptions);
 				}
 			});
-
+			
 		}
-
+		
 		// Check for missing images and resize
 		$container.find('.statement img').each(function forEachImage() {
 			//if ( $(this).attr('src') == '' ) /*$(this).remove();*/alert("foo");
@@ -198,7 +224,7 @@
 				width: $(this).width(),
 				height: $(this).height()
 			};
-
+			
 			if (options.forceImageSize === "height" ) {
 				if ( size.height > options.forcedImageHeight ) {
 					var ratio = (options.forcedImageHeight / size.height);
@@ -211,53 +237,49 @@
 					size.width  *= ratio,
 					size.height *= ratio;
 				}// else applyPaddingTo = "width";
-
+				
 			} else if (options.forceImageSize === "both" ) {
 				if (options.forcedImageHeight > 0 && size.height > options.forcedImageHeight) {
 					var ratio = (options.forcedImageHeight / size.height);
 					size.height *= ratio,
 					size.width  *= ratio;
 				}
-
+	
 				if (options.forcedImageWidth > 0 && size.width > options.forcedImageWidth) {
 					var ratio = (options.forcedImageWidth / size.width);
 					size.width  *= ratio,
 					size.height *= ratio;
 				}
+				
 
-
-			}
+			} 
 			$(this).css(size).css('margin-right', $container.find('.rank_text').outerWidth(true) + 'px');
 		});
-
-		if ( options.numberNS > 0 ){
-			if (dkActivated == 3) { // add ns to the first item
-				$('.statement').slice(0,1).addClass('ns');
-
-			} else { // add ns to last x items
-				$('.statement').slice(-options.numberNS).addClass('ns');
-			}
-		}
-
-
+		
+		// add ns to last x items
+		if ( options.numberNS > 0 ) $('.statement').slice(-options.numberNS).addClass('ns');
+		
 		for ( var i=0; i<items.length; i++ ) {
+
 			// Value of the input
 			var value = parseInt(items[i].element.val(), 10);
+			
 			var isRanked = Boolean(!isNaN(value) && value); // Verify if the value is a number greather than 0
 			if ( isRanked ) rankCount++;
 
 			$container.children('.statement:eq(' + i + ')').data('index', i).addClass(isRanked?'ranked':'').find('.rank_text').html(value);
-
+			
+			if ( isRanked ) $container.children('.statement:eq(' + i + ')').find('.otherText').show();
+				
 			if ((isRanked && (i+1) === items.length && (options.dkActivated == 1 || options.dkActivated == 2) ) || (isRanked && (i+1) === (items.length - 1) && (options.dkActivated == 2) )) {
-
+				
 				dkselected = true;
 
 				$container.children('.statement:eq(' + i + ')').find('.rank_text').html(''); // Add rank value
-
 			}
-
+			
 		};
-
+		
 		if ( options.forceResponseSize === 'no' ) {
 			$('.statement').width($('.statement').width());
 		} else if ( options.forceResponseSize === 'width' && options.forcedResponseWidth !== '' ) {
@@ -267,14 +289,15 @@
 		} else if ( options.forceResponseSize === 'both' && options.forcedResponseWidth !== '' && options.forcedResponseHeight !== '' ) {
 			$('.statement').width( options.forcedResponseWidth ).height( options.forcedResponseHeight );
 		}
-
+		
+		//alert(dkselected);
 		if((!dkselected && animatedResponses) || (!dkselected && layout === "fitRows")){
 			$container.isotope(istopeOptions);
-		}
+		}	
 		//$container.delegate('.statement', 'click', selectStatement);
 			$container.on('click', '.statement', selectStatement);
-
-
+		
+		
 		function showMoveOptions(e) {
 			$(e.delegateTarget).append('<div class="rerankContainer"><div class="upRank"></div><div class="downRank"></div></div>');
 			var marginAdjustmentTB = Math.floor( ($('.rerankContainer').outerHeight() - 17 )/2 ),
@@ -283,24 +306,24 @@
 			$('.upRank').click(rankup);
 			$('.downRank').click(rankdown);
 			// hide ranking options after 2 seconds
-			setTimeout( hideMoveOptions, 2000);
+			setTimeout( hideMoveOptions, 2000); 
 		}
-
+		
 		function hideMoveOptions(e) {
 			$( ".rerankContainer" ).remove();
 		}
-
+		
 		function rankup(e) {
-
+		
 			e.stopPropagation();
-
+		
 			var $target = $(e.delegateTarget).parents('.statement'),
 				index	= $target.data('index'),
 				$input = items[index].element,
 				value	= parseInt($input.val(), 10);
 
 			if (!value) { // item is currently UNranked
-
+							
 				if ((rankCount + 1) <= options.setMax) {
 					rankCount += 1;
 					$input.val(rankCount);
@@ -309,9 +332,9 @@
 						.find('.rank_text').html(rankCount); // Add rank value
 				}
 			} else { // item is currently ranked
-
+				
 				if ( value > 1 ) {
-
+					
 					value  -= 1;
 
 					// Rerank other items
@@ -319,47 +342,48 @@
 						var $current = $(this),
 							currentItem  = items[$current.data('index')],
 							currentValue = parseInt(currentItem.element.val(), 10);
-
+	
 						if ($current !== $target && currentValue && currentValue == value) {
 							// Re-number item
 							currentItem.element.val(currentValue + 1);
 							$current.find('.rank_text').html(currentValue + 1);
 						}
-
+	
 					});
-
+					
 					// Move item up a rank
 					$input.val(value);
 					$target.find('.rank_text').html(value); // Add rank value
-
+				
 				}
 
 			}
-
+			
 			// Update position using isotope
 			if (animatedResponses || layout === "fitRows") {
 				$container.isotope('updateSortData', $container.find('.statement'));
 				$container.isotope(istopeOptions);
 			}
-            if (window.askia
-                && window.arrLiveRoutingShortcut
+            if (window.askia 
+                && window.arrLiveRoutingShortcut 
                 && window.arrLiveRoutingShortcut.length > 0
                 && window.arrLiveRoutingShortcut.indexOf(options.currentQuestion) >= 0) {
                 askia.triggerAnswer();
             }
 		}
-
+		
 		function rankdown(e) {
-
+		
 			e.stopPropagation();
 
+			
 			var $target = $(e.delegateTarget).parents('.statement'),
 				index	= $target.data('index'),
 				$input = items[index].element,
 				value	= parseInt($input.val(), 10);
 
 			if (!value) { // item is currently UNranked
-
+							
 				if ((rankCount + 1) <= options.setMax) {
 					rankCount += 1;
 					$input.val(rankCount);
@@ -367,11 +391,11 @@
 					$target.addClass('ranked')                // Add ranked class
 						.find('.rank_text').html(rankCount); // Add rank value
 				}
-
+				
 			} else { // item is currently ranked
-
+				
 				if ( value < items.length && value < $('.ranked').length ) {
-
+				
 					value  += 1;
 
 					// Rerank other items
@@ -387,54 +411,86 @@
 						}
 
 					});
-
+					
 					// Move item up a rank
 					$input.val(value);
 					$target.find('.rank_text').html(value); // Add rank value
-
+					
 				}
 
 			}
-
+			
 			// Update position using isotope
 			if (animatedResponses || layout === "fitRows") {
 				$container.isotope('updateSortData', $container.find('.statement'));
 				$container.isotope(istopeOptions);
 			}
-            if (window.askia
-                && window.arrLiveRoutingShortcut
+            if (window.askia 
+                && window.arrLiveRoutingShortcut 
                 && window.arrLiveRoutingShortcut.length > 0
                 && window.arrLiveRoutingShortcut.indexOf(options.currentQuestion) >= 0) {
                 askia.triggerAnswer();
             }
 		}
-
+		
 		// Assign hover function
 		if ( showRankMoveControls ) {
 			if ( !((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i))) ) {
 				$container.find('.statement').hover(showMoveOptions, hideMoveOptions);
 			}
 		}
-
+		
+		function writeText() {
+			$( '#'+otherQIDarray[parseInt($(this).data('id'))-1] ).val( $(this).val() );
+            if (window.askia 
+                && window.arrLiveRoutingShortcut 
+                && window.arrLiveRoutingShortcut.length > 0
+                && window.arrLiveRoutingShortcut.indexOf(options.currentQuestion) >= 0) {
+                askia.triggerAnswer();
+            }
+		
+		}
+		
+		$( '.otherText' ).focus(function(srcc) {
+			if ($(this).val() == $(this)[0].title) {
+				$(this).removeClass("defaultTextActive");
+				$(this).val("");
+			}
+		});
+		
+		$( '.otherText' ).blur(function() {
+			if ($(this).val() == "") {
+				$(this).addClass("defaultTextActive");
+				$(this).val($(this)[0].title);
+			}
+		});
+		
+		$(this).parents('.controlContainer').find( '.otherText' ).blur();  
+				
+		
+		$(this).parents('.controlContainer').find( '.otherText' ).keyup(writeText).click(function(e) {
+			e.stopPropagation();
+		});
+		
 		if ( total_images > 0 ) {
 			$container.find('img').each(function() {
 				var fakeSrc = $(this).attr('src');
 				$("<img/>").css('display', 'none').load(function() {
 					images_loaded++;
 					if (images_loaded >= total_images) {
-
+						
 						// now all images are loaded.
 						// $container.css('visibility','visible');
-
+	
 					}
 				}).attr("src", fakeSrc);
 			});
 		} else {
 			// $container.css('visibility','visible');
 		}
-
+        
         setTimeout(function(){ document.querySelector("#adc_" + options.instanceId).style.visibility = 'visible'; }, 300);
-
+		
 		return this;
 	}
 
